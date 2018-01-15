a6d99242bcc24013284fb3b19655cde3574d9c98
Gabby Josebachvili <gabbyjose@gmail.com>
2018-01-15 11:11:47 -0500

    midway commit

--- bash.js
+++ bash.js
@@ -1,1 +1,21 @@
-console.log(process);
+//console.log(Object.keys(process));
+
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+  var cmd = data.toString().trim(); // remove the newline
+  var output;
+
+  if (cmd === 'pwd'){
+    output = process.cwd();
+  }
+  else if (cmd === 'date'){
+    output = new Date().toString();
+  }
+
+  process.stdout.write(output);
+  process.stdout.write('\nprompt > ');
+
+});
+
